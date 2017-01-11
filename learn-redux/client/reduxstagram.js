47c819ee4e21c53eff79e9529f9208369af881fa
anita <asovay@gmail.com>
2017-01-11 11:34:18 -0500

    setup components and routing

--- learn-redux/client/reduxstagram.js
+++ learn-redux/client/reduxstagram.js
@@ -7,5 +7,19 @@ import css from './styles/style.styl'
 
 // Components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main />, document.getElementById('root'));
+// React Router Deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
+const router = (
+	<Router history={browserHistory}>
+		<Route path="/" component={Main}>
+			<IndexRoute component={PhotoGrid} />
+			<Route path="/view/:postId" component={Single} />
+		</Route>
+	</Router>
+)
+
+render(router, document.getElementById('root'));
