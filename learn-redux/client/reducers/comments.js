3004111db5edf2330199f20c68a3327eb64e6375
anita <asovay@gmail.com>
2017-01-11 17:45:11 -0500

    comments, single photo, posts, store setup

--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -1,5 +1,32 @@
+function postComments(state = [], action) {
+	switch(action.type) {
+		case 'ADD_COMMENT':
+			// return new state with new comment
+			return [...state,{
+				user: action.author,
+				text: action.comment
+			}];
+		case 'REMOVE_COMMENT':
+			// return without deleted comment
+			return [
+				...state.slice(0, action.index), // from start to deleted comment
+				...state.slice(action.index + 1) // after deleted comment to end
+			]
+		default:
+			return state;
+	}
+}
+
 function comments(state = [], action) {
-	console.log(state, action);
+	if (typeof action.postId !== 'undefined') {
+		return {
+			// take current state
+			...state,
+			// overwrite this post with new one
+			[action.postId]: postComments(state[action.postId], action)
+		}
+	}
+
 	return state;
 }
 
--- learn-redux/client/reducers/posts.js
+++ learn-redux/client/reducers/posts.js
@@ -4,9 +4,18 @@
 // 2. copy of current state
 
 function posts(state = [], action) {
-	console.log("The post will change");
-	console.log(state, action);
-	return state;
+	switch(action.type) {
+		case 'INCREMENT_LIKES':
+			// return the updated state
+			const index = action.index;
+			return [
+				...state.slice(0, index), // before the one we are updating
+				{...state[index], likes: state[index].likes + 1}, // the one to update
+				...state.slice(index + 1) // after the one we are updating
+			]
+		default:
+			return state;
+	}
 }
 
 export default posts;
